d2d56c8743e5d1c8ec38f159e0b536f3ee8f46b6
fishx <2690866872@qq.com>
2023-08-10 20:05:52 +0800

    完成修改功能

--- src/main/java/com/phone/www/controller/HandsetController.java
+++ src/main/java/com/phone/www/controller/HandsetController.java
@@ -1,10 +1,15 @@
 package com.phone.www.controller;
 
+import com.phone.www.entity.Handset;
 import com.phone.www.service.HandsetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HandsetController {
@@ -14,5 +19,23 @@ public class HandsetController {
   @GetMapping("/")
   public String indexPage() {
     return "index";
+  }
+
+  @GetMapping("/update/{id}")
+  public String updatePage(@PathVariable Integer id, HttpServletRequest request) {
+    request.setAttribute("phone", handsetService.query().eq("hsId", id).one());
+    return "update";
+  }
+
+  @GetMapping("/result.html")
+  public String toResultPage() {
+    return "result";
+  }
+
+  @PostMapping("/update")
+  public String update(Handset handset, HttpServletRequest request) {
+    String msg = handsetService.updateById(handset) ? "修改成功" : "修改失败";
+    request.getSession().setAttribute("msg", msg);
+    return "redirect:result.html";
   }
 }
