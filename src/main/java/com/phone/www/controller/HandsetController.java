e735a601d1224b1763a75a563c0acacc00edb5aa
zr <3483793802@qq.com>
2023-08-11 09:20:15 +0800

    add 查询功能

--- src/main/java/com/phone/www/controller/HandsetController.java
+++ src/main/java/com/phone/www/controller/HandsetController.java
@@ -3,39 +3,50 @@ package com.phone.www.controller;
 import com.phone.www.entity.Handset;
 import com.phone.www.service.HandsetService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class HandsetController {
-  @Resource
-  HandsetService handsetService;
+    @Resource
+    HandsetService handsetService;
 
-  @GetMapping("/")
-  public String indexPage() {
-    return "index";
-  }
+    @GetMapping("/")
+    public String indexPage() {
+        return "select";
+    }
 
-  @GetMapping("/update/{id}")
-  public String updatePage(@PathVariable Integer id, HttpServletRequest request) {
-    request.setAttribute("phone", handsetService.query().eq("hsId", id).one());
-    return "update";
-  }
+    @GetMapping("/update/{id}")
+    public String updatePage(@PathVariable Integer id, HttpServletRequest request) {
+        request.setAttribute("phone", handsetService.query().eq("hsId", id).one());
+        return "update";
+    }
 
-  @GetMapping("/result.html")
-  public String toResultPage() {
-    return "result";
-  }
+    @GetMapping("/result.html")
+    public String toResultPage() {
+        return "result";
+    }
 
-  @PostMapping("/update")
-  public String update(Handset handset, HttpServletRequest request) {
-    String msg = handsetService.updateById(handset) ? "修改成功" : "修改失败";
-    request.getSession().setAttribute("msg", msg);
-    return "redirect:result.html";
-  }
+    @PostMapping("/update")
+    public String update(Handset handset, HttpServletRequest request) {
+        String msg = handsetService.updateById(handset) ? "修改成功" : "修改失败";
+        request.getSession().setAttribute("msg", msg);
+        return "redirect:result.html";
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<Handset> list() {
+        return handsetService.list();
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, HttpServletRequest request) {
+        String msg = handsetService.removeById(id) ? "删除成功" : "删除失败";
+        request.getSession().setAttribute("msg", msg);
+        return "redirect:result.html";
+    }
 }
